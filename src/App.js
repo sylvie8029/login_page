0058093e6af69d2e7132a17e5bc6ccc06bb84018
sylvie8029 <yawei8029@gmail.com>
2018-12-23 20:21:24 -0500

    login-page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,111 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container">
+        <div className="login">
+          <div className="login--panel">
+            <header className="login--panel--header">
+              <img
+                src={logo}
+                className="login--panel--header-logo"
+                alt="logo"
+              />
+              <div className="login--panel--header-title">
+                <p>Liquidity Manager</p>
+              </div>
+            </header>
+            <form className="login--panel--form">
+              <div className="login--panel--form-control">
+                <label className="login--panel--form-control-label">
+                  Username:
+                </label>
+                <input className="login--panel--form-control-input" />
+              </div>
+              <div className="login--panel--form-control">
+                <label className="login--panel--form-control-label">
+                  Password:
+                </label>
+                <input className="login--panel--form-control-input" />
+              </div>
+              <div className="login--panel--form-control">
+                <input
+                  type="checkbox"
+                  className="login--panel--form-checkbox"
+                />
+                Remember Me
+              </div>
+              <div className="login--panel--form-button-container">
+                <button className="login--panel--form-button-container--button">
+                  Login
+                </button>
+              </div>
+            </form>
+            <footer className="login--panel--footer">
+              <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                ForgotPassword
+              </a>
+            </footer>
+          </div>
+        </div>
+        <div className="login-2">
+          <div className="login-2--panel">
+            <header className="login-2--panel--header">
+              <img
+                src={logo}
+                className="login-2--panel--header-logo"
+                alt="logo"
+              />
+              <div className="login-2--panel--header-title">
+                <p>Liquidity Manager</p>
+              </div>
+            </header>
+            <form className="login-2--panel--form">
+              <div className="login-2--panel--form-control">
+                <label className="login-2--panel--form-control-label">
+                  Username:
+                </label>
+                <input className="login-2--panel--form-control-input" />
+              </div>
+              <div className="login-2--panel--form-control">
+                <label className="login-2--panel--form-control-label">
+                  Password:
+                </label>
+                <input className="login-2--panel--form-control-input" />
+              </div>
+              <div className="login-2--panel--form-control">
+                <input
+                  type="checkbox"
+                  className="login-2--panel--form-checkbox"
+                />
+                Remember Me
+              </div>
+              <div className="login-2--panel--form-button-container">
+                <button className="login-2--panel--form-button-container--button">
+                  Login
+                </button>
+              </div>
+            </form>
+            <footer className="login-2--panel--footer">
+              <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                ForgotPassword
+              </a>
+            </footer>
+          </div>
+        </div>
       </div>
     );
   }
